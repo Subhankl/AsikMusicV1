409cc94d5272f7a0d445c28b9a35e9161312c570
Subhankl <106168797+Subhankl@users.noreply.github.com>
2022-07-23 14:36:51 +0400

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -14,7 +14,7 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "❤️‍🔥 Məni qrupa əlavə et ❤️‍🔥", url=f"https://t.me/CraftyMusicBot?startgroup=true"
+                        "❤️‍🔥 Məni qrupa əlavə et ❤️‍🔥", url=f"https://t.me/TTKMusic_bot?startgroup=true"
                     )
                 ],
                 [
@@ -22,7 +22,7 @@ async def start(_, message: Message):
                         "🐊 komandalar" , callback_data= "herkes"
                     ),
                     InlineKeyboardButton(
-                        "🇦🇿 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url="https://t.me/CraftySup"
+                        "🇦🇿 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url="https://t.me/TTK_resmi"
                     )
                 ],
                 [
@@ -44,10 +44,10 @@ async def herkes(_, query: CallbackQuery):
              [
                  [
                      InlineKeyboardButton(
-                         "🇹🇷 𝖠𝗌𝗂𝗌𝗍𝖺𝗇", url="https://t.me/CraftyAsistan"
+                         "🇹🇷 𝖠𝗌𝗂𝗌𝗍𝖺𝗇", url="https://t.me/TTKAsistant"
                      ),
                      InlineKeyboardButton(
-                         "🧑🏻‍💻 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/iamZ3NT4"
+                         "🧑🏻‍💻 𝖮𝗐𝗇𝖾𝗋", url="https://t.me/FRESTYSHT"
                      )
                  ],
                  [
@@ -67,7 +67,7 @@ async def cbstart(_, query: CallbackQuery):
             [
                 [
                     InlineKeyboardButton(
-                        "🎉 𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾 🎉", url=f"https://t.me/CraftyMusicBot?startgroup=true"
+                        "🎉 𝖡𝖾𝗇𝗂 𝖦𝗋𝗎𝖻𝖺 𝖤𝗄𝗅𝖾 🎉", url=f"https://t.me/TTKMusic_bot?startgroup=true"
                     )
                 ],
                 [
@@ -75,7 +75,7 @@ async def cbstart(_, query: CallbackQuery):
                         "📚 𝖪𝗈𝗆𝗎𝗍𝗅𝖺𝗋" , callback_data= "herkes"
                     ),
                     InlineKeyboardButton(
-                        "🇹🇷 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url=f"https://t.me/CraftySup"
+                        "🇹🇷 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url=f"https://t.me/TTK_resmi"
                     )
                 ],
                 [
